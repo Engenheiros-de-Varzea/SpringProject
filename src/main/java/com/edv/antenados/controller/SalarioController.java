772964d2fc8c8486a48e7bbca316031ccc52c703
PHCarmo <paulo.henrique.carmo@hotmail.com>
2020-05-31 13:50:59 -0300

    Adicionado vinculo entre servidor e salário

--- src/main/java/com/edv/antenados/controller/SalarioController.java
+++ src/main/java/com/edv/antenados/controller/SalarioController.java
@@ -1,8 +1,11 @@
 package com.edv.antenados.controller;
 
+import com.edv.antenados.model.Salario;
 import com.edv.antenados.repository.Salarios;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +25,14 @@ public class SalarioController {
     public ModelAndView pesquisa() {
         ModelAndView mv = new ModelAndView("/salario/ListagemSalarios");
         mv.addObject("salarios", salarios.findAll());
+        
+        return mv;
+    }
+    
+    @RequestMapping("/{id_servidor}")
+    public ModelAndView visualizar(@PathVariable Integer id_servidor) {
+        ModelAndView mv = new ModelAndView("/salario/ListagemSalarios");
+        mv.addObject("salarios", salarios.findByServidor(id_servidor));
         
         return mv;
     }
--- src/main/java/com/edv/antenados/repository/Salarios.java
+++ src/main/java/com/edv/antenados/repository/Salarios.java
@@ -1,8 +1,13 @@
 package com.edv.antenados.repository;
 
 import com.edv.antenados.model.Salario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface Salarios extends JpaRepository<Salario, Integer> {
+    
+    @Query(value = "SELECT * FROM salario s WHERE ID_SERVIDOR = ?1", nativeQuery = true)
+    public List<Salario> findByServidor(Integer id_servidor);
     
 }
