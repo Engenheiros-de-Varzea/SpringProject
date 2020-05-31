c388d580f317d210d6d70a5d5e5bb877bf84af7d
PHCarmo <paulo.henrique.carmo@hotmail.com>
2020-05-31 13:49:13 -0300

    Criado webconfig para as páginas de erro

---
+++ src/main/java/com/edv/antenados/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.edv.antenados.config;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.support.DomainClassConverter;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> 
+            {
+                container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/403"));
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+                container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));
+            }
+        );
+    }
+    
+}
