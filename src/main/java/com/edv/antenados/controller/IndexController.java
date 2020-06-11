7aea80b4aaf4e5c30dccfaf6b0b1d95edf362206
George Wurthmann <george.wurthmann@fatec.sp.gov.br>
2020-06-10 22:38:01 -0300

    Página Index, Icon, Logo, Autenticação
    Inclusão:
    - Inclusão da página Index e seu controlador (somente a logo presente);
    - Inclusão do favicon.ico.
    
    Alterações:
    - Alteração dos PNGs da logo;
    - Reorganização do menu para a inclusão do Home;
    - Alteração do CSS para o menu e footer responsivo, e identação de alguns itens;
    - Correção da disponibilidade dos botões do menu que verificam autenticação nas páginas de erro.

--- src/main/java/com/edv/antenados/controller/IndexController.java
+++ src/main/java/com/edv/antenados/controller/IndexController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TrendsController {
+public class IndexController {
     
-    @RequestMapping("/trends")
+    @RequestMapping("/index")
     public String screenRecommendation() {
-        return "/administrador/Trends";
+        return "/index/Index";
     }
     
 }
--- src/main/java/com/edv/antenados/controller/LoginController.java
+++ src/main/java/com/edv/antenados/controller/LoginController.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.edv.antenados.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-/**
- *
- * @author teste
- */
+
 @Controller
 public class LoginController {
     
