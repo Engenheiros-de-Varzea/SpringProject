478401fa53a19aa083fdff5047add1652476010d
George Wurthmann <george.wurthmann@fatec.sp.gov.br>
2020-06-14 18:42:26 -0300

    Atualização do Front do Index
    Somente modificação no frontend, necessário fazer o backend e ajustar o front para as funcionalidades.
    
    Na lateral esquerda:
    - Inclusão da busca recomendada.
    
    No centro:
    - Inclusão do filtro mês de referência;
    - Inclusão da seleção por Cargo / Salário;
    - Inclsuão do input e botão de busca.

--- src/main/java/com/edv/antenados/controller/IndexController.java
+++ src/main/java/com/edv/antenados/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.edv.antenados.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String screenRecommendation() {
         return "/index/Index";
     }
     
+    @RequestMapping("/") 
+    public String home(){
+        return "redirect:index"; 
+    }
 }
