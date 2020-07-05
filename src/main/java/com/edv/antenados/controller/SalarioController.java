29c9728eb7cbf18df756a20df974aa6da79b2d1b
PHCarmo <paulo.henrique.carmo@hotmail.com>
2020-07-05 00:23:24 -0300

    Remoção dos controladores de listagem de salários

--- src/main/java/com/edv/antenados/controller/SalarioController.java
+++ src/main/java/com/edv/antenados/controller/SalarioController.java
@@ -1,16 +1,9 @@
 package com.edv.antenados.controller;
 
-import com.edv.antenados.model.Salario;
 import com.edv.antenados.repository.Salarios;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/salarios")
@@ -18,21 +11,5 @@ public class SalarioController {
     
     @Autowired
     private Salarios salarios;
-    
-    @RequestMapping
-    public ModelAndView pesquisa(Pageable pageable) {
-        ModelAndView mv = new ModelAndView("/salario/ListagemSalarios");
-        mv.addObject("salarios", salarios.findAll(pageable));
-
-        return mv;
-    }
-
-    @RequestMapping("/{id_servidor}")
-    public ModelAndView visualizar(@PathVariable Integer id_servidor) {
-        ModelAndView mv = new ModelAndView("/salario/ListagemSalarios");
-        mv.addObject("salarios", salarios.findSalarioByServidor(id_servidor));
-        
-        return mv;
-    }
-    
+            
 }
